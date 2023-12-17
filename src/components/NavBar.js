78830679535fff2efd3297c5d04ac60350474ce5
hd <classylifestyling@gmail.com>
2023-12-17 21:41:42 +0530

    done 5

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Link,useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavBar = () => {
   let location = useLocation();
 
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light" style={{backgroundColor:'white'}}>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-light" style={{ backgroundColor: 'white' }}>
         <div className="container-fluid">
           <Link className="navbar-brand" to="/"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" className="bi bi-newspaper mx-2 mb-1 ml-5" viewBox="0 0 16 16">
             <path d="M0 2.5A1.5 1.5 0 0 1 1.5 1h11A1.5 1.5 0 0 1 14 2.5v10.528c0 .3-.05.654-.238.972h.738a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 1 1 0v9a1.5 1.5 0 0 1-1.5 1.5H1.497A1.497 1.497 0 0 1 0 13.5zM12 14c.37 0 .654-.211.853-.441.092-.106.147-.279.147-.531V2.5a.5.5 0 0 0-.5-.5h-11a.5.5 0 0 0-.5.5v11c0 .278.223.5.497.5z" />
@@ -19,15 +19,15 @@ const NavBar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  { console.log(location.pathname)}
+                  {console.log(location.pathname)}
                   <Link id="myactive" className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">Home</Link>
                 </li>
-                <li className="nav-item"><Link  id="myactive" className={`nav-link ${location.pathname === "/Business" ? "active" : ""} `}  to="/Business">Business</Link></li>
-                <li className="nav-item"><Link  id="myactive" className={`nav-link ${location.pathname === "/Entertainment" ? "active" : ""} `}  to="/Entertainment">Entertainment</Link></li>
-                <li className="nav-item"><Link  id="myactive" className={`nav-link ${location.pathname === "/Health" ? "active" : ""} `}  to="/Health">Health</Link></li>
-                <li className="nav-item"><Link  id="myactive" className={`nav-link ${location.pathname === "/Science" ? "active" : ""} `}  to="/Science">Science</Link></li>
-                <li className="nav-item"><Link  id="myactive" className={`nav-link ${location.pathname === "/Sports" ? "active" : ""} `}  to="/Sports">Sports</Link></li>
-                <li className="nav-item"><Link  id="myactive" className={`nav-link ${location.pathname === "/Technology" ? "active" : ""} `}  to="/Technology">Technology</Link></li>
+                <li className="nav-item"><Link id="myactive" className={`nav-link ${location.pathname === "/Business" ? "active" : ""} `} to="/Business">Business</Link></li>
+                <li className="nav-item"><Link id="myactive" className={`nav-link ${location.pathname === "/Entertainment" ? "active" : ""} `} to="/Entertainment">Entertainment</Link></li>
+                <li className="nav-item"><Link id="myactive" className={`nav-link ${location.pathname === "/Health" ? "active" : ""} `} to="/Health">Health</Link></li>
+                <li className="nav-item"><Link id="myactive" className={`nav-link ${location.pathname === "/Science" ? "active" : ""} `} to="/Science">Science</Link></li>
+                <li className="nav-item"><Link id="myactive" className={`nav-link ${location.pathname === "/Sports" ? "active" : ""} `} to="/Sports">Sports</Link></li>
+                <li className="nav-item"><Link id="myactive" className={`nav-link ${location.pathname === "/Technology" ? "active" : ""} `} to="/Technology">Technology</Link></li>
               </ul>
 
             </div>
