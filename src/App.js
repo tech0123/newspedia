23ea756a829895e25c0707eacf93fcbc38c431ee
hd <classylifestyling@gmail.com>
2023-12-17 19:27:13 +0530

    done 3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import FetchArticles from './components/FetchArticles';
+// import FetchArticles from './components/FetchArticles';
 
 
 const App = () => {
