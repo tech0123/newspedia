15e89f48d390695b5f873a4d2ce56b6bd663a610
hd <classylifestyling@gmail.com>
2023-12-17 19:30:55 +0530

    done 4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 import News from './components/News';
 
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
